f7fd6bb5e9d3ed543c8d58199941bed6a271abc9
rabbitSamurai <90148070+rabbitSamurai@users.noreply.github.com>
2023-03-06 21:26:01 -0800

    prova de logica de javascript

--- script.js
+++ script.js
@@ -1,14 +1,43 @@
+function CalcuIMC() {
 
-function celsiustoFarenheit () {
-    let celsius = document.getElementById('inputCelsius').value;
-    celsiusCalculo = parseInt(celsius) + 32;
-    console.log(celsiusCalculo)
-    document.getElementById('span').innerHTML = celsiusCalculo + ' em Farenheit';
+    let peso = document.getElementById('peso').value;
+    let altura = document.getElementById('altura').value;
+
+    imc = peso / (altura) **2
+    console.log(imc)
+
+    return imc
 }
 
-function farenheitToCelsius (){
-    let farenheit = document.getElementById('inputFarenheit').value;
-    farenheitCalculo = parseInt(farenheit) - 32 * 5/9
-    console.log(farenheitCalculo)
-    document.getElementById('spanf').innerHTML = farenheitCalculo + 'em celsius'
+
+
+function Obesidade(){
+    let imprimir
+
+    CalcuIMC()
+
+    if (imc < 18.5){
+
+        imprimir = 'magro'
+
+    }
+
+    else if(imc > 18.6 && imc < 24.9){
+        imprimir = 'normal'
+    }
+    else if(imc > 25 && imc < 29.9){
+        imprimir = 'Sobrepeso'
+    }
+
+    else if(imc > 30){
+        imprimir = 'obeso'
+    }
+
+    document.getElementById('resultImc').innerHTML = Math.round(imc)
+    document.getElementById('resultObesidade').innerHTML = imprimir
+
+    console.log(imprimir)
+    return imprimir
 }
+
+
